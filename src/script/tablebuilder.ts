5f268a6a8343d1c224b5d1f463befbe12d1eea3e
AsPulse <aspulse.x@gmail.com>
2022-01-29 12:07:25 +0900

    Create tablebuilder.ts

---
+++ src/script/tablebuilder.ts
@@ -0,0 +1,21 @@
+import { IPrefecture } from './interfaces/prefecture';
+
+export function tableBuild(
+  targets: IPrefecture[],
+  parameters: (keyof IPrefecture)[]
+) {
+  const table = document.createElement('table');
+  const data = targets.map((v) => parameters.map((e) => v[e] ?? '-'));
+  data
+    .map((v) => {
+      const tr = document.createElement('tr');
+      v.map((e) => {
+        const td = document.createElement('td');
+        td.textContent = `${e}`;
+        return td;
+      }).forEach((j) => tr.appendChild(j));
+      return tr;
+    })
+    .forEach((v) => table.appendChild(v));
+  return table;
+}
