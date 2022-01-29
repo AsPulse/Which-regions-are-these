36d9a55c95800a1a9093baacb1b96f79ce4de084
AsPulse <aspulse.x@gmail.com>
2022-01-29 12:34:30 +0900

    Add: question-sentence

--- src/script/index.ts
+++ src/script/index.ts
@@ -8,12 +8,27 @@ import { randomChoice } from './util/randomChoice';
 const storage = new localStorage<IStorage>({});
 storage.save();
 document.addEventListener('DOMContentLoaded', () => {
-  document
-    .getElementById('question')
-    ?.appendChild(
-      tableBuild(randomChoice(prefectureData, 5).map((v, i) =>{
+  const countOfQuiz = 5;
+  const target = randomChoice(prefectureData, countOfQuiz);
+
+  const sentence = document.getElementById('sentence');
+  if (sentence)
+    sentence.innerHTML = `これは、${countOfQuiz}つの都道府県に関する表で、<br>${nthAlphabet(
+      0,
+      true
+    )}から${nthAlphabet(countOfQuiz - 1, true)}はそれぞれ<b>${target
+      .slice()
+      .sort(() => Math.random() - 0.5)
+      .map((v) => v.name)
+      .join('、')}</b>です。<br>それぞれに当てはまる県名を答えなさい。`;
+
+  document.getElementById('question')?.appendChild(
+    tableBuild(
+      target.map((v, i) => {
         v.name = nthAlphabet(i, true);
         return v;
-      }), ['name', 'population'])
-    );
+      }),
+      ['name', 'population']
+    )
+  );
 });
