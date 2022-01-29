f49dc7b09788110fa8d8dfe8efef98930da11031
AsPulse <aspulse.x@gmail.com>
2022-01-29 12:16:13 +0900

    Add: tester

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,5 +1,15 @@
+import { prefectureData } from './data/prefecture';
 import { IStorage } from './interfaces/storage';
 import { localStorage } from './localStorage';
+import { tableBuild } from './tablebuilder';
+import { randomChoice } from './util/randomChoice';
 
 const storage = new localStorage<IStorage>({});
 storage.save();
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .getElementById('question')
+    ?.appendChild(
+      tableBuild(randomChoice(prefectureData, 5), ['name', 'population'])
+    );
+});
